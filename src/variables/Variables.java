5f28bde0767aa251bf6f9181a1a8fe58b7393a32
NAVTTC07 <NAVTTC07@iba.edu.pk>
2021-01-13 14:46:30 +0500

    Testing repo

---
+++ src/variables/Variables.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author NAVTTC07
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        
+        int number1=20;
+        int number2=10;//--camel case firstNumberStore
+        int ans=number1+number2;//8+10=18
+        
+        int number3=9, number4=7 ,number5=6;
+        
+        System.out.print("lEARNING VARIABLES");
+        System.out.println("number1");
+        System.out.println(number1);
+        System.out.println(number2);
+        //Number 1 = 5 ,Number 2= 10, Answer = 15
+        
+        //1st
+        System.out.print("Number 1 = "+number1);
+        System.out.println("  Number 2 = "+number2);
+        
+        
+        //2nd
+       
+        System.out.println("Number 1 = "+number1 + ", Number 2 = "+number2 + ", Answer = "+ans);
+        System.out.println("Number 1 = "+number1 + ", Number 2 = "+number2 + ", Answer = "+(number1+number2));//concatenation
+        
+        //Number 1=5 , NUmber 2=3 , Subtraction Of Number 1 And Number 2 is 2
+        
+        double num1=5.5;
+        System.out.println("Decimal value of num1 is "+num1);
+        
+        char alphabet='A';
+        
+        String name ="hello";
+        
+        System.out.println("Alphabet is "+ alphabet);
+        System.out.println("Name is "+name);
+        
+        float num2=5.3f;
+        
+        boolean check=true;
+        boolean check1=false;
+      
+        //Output
+        //Name is Ali
+        //Age is 20
+        //Height is 5.5
+        //Grade is A
+        
+
+        
+    }
+    
+}
